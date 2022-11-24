dc92cdae5a42aaf3bde10163673ae23bcd0e2414
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2022-11-24 14:13:24 -0300

    mudança total main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import home from"./page/home.js"
 import gerarLinks from"./page/gerarLinks.js"
 import minisite from "./page/minisite.js"
+import areaAdm from "./page/areaAdm.js"
 import {router, navigateTo}  from"./lib/index.js"
 
 const URL = location.href.includes("?") 
+
+
 
 
 //Auth
@@ -11,7 +14,7 @@ if (URL) {
      router({minisite})  
 } else if(localStorage.hasOwnProperty("dono")) { 
      navigateTo("/gerarLinks")
-     router({gerarLinks, minisite})
+     router({gerarLinks, minisite, areaAdm})
 } else{    
      navigateTo("/home")
      router({home})
