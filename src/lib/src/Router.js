1ebd9f1d83cff20fd2d9bbf5bf95059657db0650
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2022-12-01 19:07:02 -0300

    Roteamento por função, sem URL(pathname)

--- src/lib/src/Router.js
+++ src/lib/src/Router.js
@@ -1,33 +1,15 @@
 
-import navigateTo from"./navigateTo.js"
-import Render from "./Render.js"
 
-export default function Router(Paginas){  
-       const pathname = window.document.location.pathname.replace(/^.*\//g, '')     
-      // const hash = window.location.hash.replace(/([?][A-z]*)([=]).*/, "")
-      // const filterHash = hash.replace("#", "")
-    
- 
-       Paginas[pathname] ? Render(Paginas[pathname]) : Render(Paginas["home"])   
 
-    
-       window.addEventListener("popstate", Router)
-       
-       window.document.addEventListener("DOMContentLoaded", () => {
-
-           document.body.addEventListener("click", e => {
-
-               if(e.target.matches("[ali-link]")){
-                 e.preventDefault()
-                 navigateTo(e.target.href)
-                }
-           })
-       })
-
-          
-    
+export default function Router(Pagina){ 
+    const dataPaginaName = Object.keys(Pagina)[0]   
+    console.log(dataPaginaName)           
+   
+    window.document.querySelector("#app").innerHTML =  Pagina[dataPaginaName]()
 
     
  }
+
+
 
 
