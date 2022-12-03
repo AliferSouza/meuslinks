51944cae03d9a8139fd5c951274502fa55754539
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2022-12-02 22:28:44 -0300

    Update Router.js

--- src/lib/src/Router.js
+++ src/lib/src/Router.js
@@ -3,7 +3,7 @@
 
 export default function Router(Pagina){ 
     const dataPaginaName = Object.keys(Pagina)[0]   
-    console.log(dataPaginaName)           
+      
    
     window.document.querySelector("#app").innerHTML =  Pagina[dataPaginaName]()
 
